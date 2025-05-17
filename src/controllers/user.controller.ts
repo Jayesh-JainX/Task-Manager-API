971a9e68bb7a04d57a2366e440ceba7140832279
Jayesh Jain <103871719+Jayesh-JainX@users.noreply.github.com>
2025-05-17 09:35:36 +0530

    task-manager

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { supabase } from "../config/supabase";
+import { supabase } from "../config/supabase.js";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET =
