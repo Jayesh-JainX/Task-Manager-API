46380e19093aedb021d73ec76d17465df93114a3
Jayesh Jain <103871719+Jayesh-JainX@users.noreply.github.com>
2025-05-17 10:17:50 +0530

    task-manager

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 import { scheduleTaskAutoClose } from "./controllers/task.controller.js";
 import { setupSwagger } from "./config/swagger.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 setupSwagger(app);
 
